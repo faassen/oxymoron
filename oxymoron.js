de30176317fba29fdcf9aeaf208fdfae62805e9e
faassen <faassen@startifact.com>
2014-04-30 16:15:47 +0200

    Some basic HTML to React.DOM parsing code and test.

--- oxymoron.js
+++ oxymoron.js
@@ -1,1 +1,32 @@
 /* */
+var htmlparser = require('htmlparser2');
+var React = require('react');
+var dom = React.DOM;
+
+exports.compile = function(s) {
+    var topBuilt = [];
+    var stack = [{built: topBuilt}];
+
+    var parser = new htmlparser.Parser({
+        onopentag: function(name, attribs) {
+            stack.push({name: name, attribs: attribs, built: []});
+        },
+        ontext: function(text) {
+            stack[stack.length - 1].built.push(text);
+        },
+        onclosetag: function(tagname) {
+            var top = stack.pop();
+            var built = top.built;
+            if (built.length == 1) {
+                built = built[0];
+            }
+            stack[stack.length -1].built.push(
+                React.DOM[top.name](top.attribs, built));
+        }
+    });
+    parser.write(s);
+    parser.end();
+    // topBuilt should be 1 entry
+    return topBuilt[0];
+};
+
--- test/test_riposte.js
+++ test/test_riposte.js
@@ -1,8 +1,20 @@
 var chai = require('chai');
+var oxymoron = require('../oxymoron');
+var React = require('react');
+
 var assert = chai.assert;
 
-suite("Foo", function() {
-    test("blah", function() {
-        assert.equal('foo', 'bar');
+suite("oyxmoron", function() {
+    test("parse HTML with text", function() {
+        var r = oxymoron.compile('<p>Hello world!</p>');
+        assert.deepEqual(r, React.DOM.p(null, "Hello world!"));
+    });
+    test("parsing HTML with mixed text", function() {
+        var r = oxymoron.compile('<p>Hello <strong>world</strong>!</p>');
+        debugger;
+        assert.deepEqual(r, React.DOM.p(null,
+                                        ["Hello ",
+                                         React.DOM.strong(null, "world"),
+                                         "!"]));
     });
 });
