47290b6fa6544f055465820c4aa138b912f8981a
faassen <faassen@startifact.com>
2014-05-02 16:01:47 +0200

    Add minifier to gruntfile. doesn't work with codebase yet.

--- gruntfile.js
+++ gruntfile.js
@@ -13,10 +13,34 @@ module.exports = function(grunt) {
                     test: true
                 }
             }
+        },
+        closureCompiler: {
+            options: {
+                compilerFile: '/home/faassen/install/closure_compiler/compiler.jar',
+                checkModified: true,
+                compilerOpts: {
+                    compilation_level: 'SIMPLE_OPTIMIZATIONS',
+                    language_in: 'ECMASCRIPT5',
+                    define: ["'goog.DEBUG=false'"],
+                    warning_level: 'verbose',
+                    summary_detail_level: 3,
+                    output_wrapper: '"(function(){%output%}).call(this);"'
+                },
+                d32: false, // will use 'java -client -d32 -jar compiler.jar'
+                TieredCompilation: true
+            },
+            targetName: {
+                src: 'oxymoron-bundled.js',
+                dest: 'oxymoron-bundled.min.js'
+
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-closure-tools');
+
+    grunt.registerTask('minify', ['closureCompiler']);
 
     grunt.registerTask('check', ['jshint']);
     grunt.registerTask('default', ['jshint']);
